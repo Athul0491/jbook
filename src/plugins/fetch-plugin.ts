1d1bedbbed40541e1c13bb0c90ab5d3e9bac55bd
Athul <65664185+Athul0491@users.noreply.github.com>
2021-10-23 03:07:02 +0530

    update

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -8,7 +8,7 @@ const fileCache = localForage.createInstance({
 
 export const fetchPlugin = (inputCode: string) => {
   return {
-    name: "fetchPlugin",
+    name: "fetch-plugin",
     setup(build: esbuild.PluginBuild) {
       build.onLoad({ filter: /.*/ }, async (args: any) => {
         console.log("onLoad", args);
@@ -20,17 +20,28 @@ export const fetchPlugin = (inputCode: string) => {
           };
         }
 
-        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
-          args.path
-        );
-        if (cachedResult) {
-          return cachedResult;
-        }
+        // const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+        //   args.path
+        // );
+        // if (cachedResult) {
+        //   return cachedResult;
+        // }
 
         const { data, request } = await axios.get(args.path);
+
+        const fileType = args.path.match(/.css$/) ? "css" : "jsx";
+        const contents =
+          fileType === "css"
+            ? `
+        const style = document.createElement('style');
+        style.innerText = 'body { background-color:"red"}';
+        document.head.appendChild(style);
+        `
+            : data;
+
         const result: esbuild.OnLoadResult = {
           loader: "jsx",
-          contents: data,
+          contents,
           resolveDir: new URL("./", request.responseURL).pathname,
         };
         await fileCache.setItem(args.path, result);
