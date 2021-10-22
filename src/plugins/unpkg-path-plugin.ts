1766e2bf9d51893cd728e0218a294755f4b71c4e
Athul <65664185+Athul0491@users.noreply.github.com>
2021-10-22 18:02:10 +0530

    implemented caching layer

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,6 +1,10 @@
 import * as esbuild from "esbuild-wasm";
 import axios from "axios";
+import localForage from "localforage";
 
+const fileCache = localForage.createInstance({
+  name: "fileCache",
+});
 export const unpkgPathPlugin = () => {
   return {
     name: "unpkg-path-plugin",
@@ -40,12 +44,21 @@ export const unpkgPathPlugin = () => {
           };
         }
 
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+        if (cachedResult) {
+          return cachedResult;
+        }
+
         const { data, request } = await axios.get(args.path);
-        return {
+        const result: esbuild.OnLoadResult = {
           loader: "jsx",
           contents: data,
           resolveDir: new URL("./", request.responseURL).pathname,
         };
+        await fileCache.setItem(args.path, result);
+        return result;
       });
     },
   };
