e005c3c7213393f68482e50f8c6b559b04c40477
Athul <65664185+Athul0491@users.noreply.github.com>
2022-01-07 21:16:15 +0530

    completed integration of redux

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,12 +1,12 @@
 import { ActionType } from "../action-types";
 import { CellTypes } from "../cell";
-
 import {
   Action,
   UpdateCellAction,
   DeleteCellAction,
   MoveCellAction,
   InsertCellBeforeAction,
+  Direction,
 } from "../actions";
 
 export const updateCell = (id: string, content: string): UpdateCellAction => {
@@ -26,10 +26,7 @@ export const deleteCell = (id: string): DeleteCellAction => {
   };
 };
 
-export const moveCell = (
-  id: string,
-  direction: "up" | "down"
-): MoveCellAction => {
+export const moveCell = (id: string, direction: Direction): MoveCellAction => {
   return {
     type: ActionType.MOVE_CELL,
     payload: {
